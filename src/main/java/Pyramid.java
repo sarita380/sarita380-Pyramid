4e17897c9f5ee305fb9a99187cd0828574cc9aed
Sara Aliaga <sarialiaga5@gmail.com>
2023-05-01 12:04:13 +0000

    Completed Challenge Lab: Pyramid

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,14 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        StringBuilder str = new StringBuilder();
+        for(int i=0; i < n; i++){
+            for(int j=0; j<i+1; j++){
+                str.append("*");
+            }
+            str.append("\n");
+        }
+
+        return str.toString();
     }
 }
